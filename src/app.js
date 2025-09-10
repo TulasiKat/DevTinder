7e3f6be7ff92508e4b1abc8ad170c1c39fe08734
tulasikatukoju-joveo <tulasi@joveo.com>
2025-09-10 16:16:09 +0530

    explored get post delete

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ const express = require("express");
 
 const app = express();
 
-app.use("/home", (req, res) => {
-    res.send("hello tulasi code code")
+app.get("/user" , (req, res)=>{
+    res.send("User : Tulasi")
 })
+
+app.post("/user" , (req, res)=>{
+    res.send("User added : Mounika")
+})
+app.delete("/user" , (req, res)=>{
+    res.send("Deleted user")
+})
+
 
 app.listen(3000, () => {
     console.log("server is running"
