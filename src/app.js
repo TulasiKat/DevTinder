0454f65d3d9f25a111964394628a208ca8dbb58a
tulasikatukoju-joveo <tulasi@joveo.com>
2025-09-17 17:17:03 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const app = express();
- 
-const {adminAuth, userAuth} = require("./middlewares/auth")
 
-app.use("/admin", adminAuth)
-
-app.get("/user/login", (req, res) => {
-  res.send("user loggedin successfully");
+app.get("/getUserData", (req, res) => {
+  throw new Error("errorred");
+  res.send("user data sent");
 });
 
-app.get("/user", userAuth, (req, res) => {
-  res.send("user got all data");
-});
-
-app.get("/admin/getAllData", (req, res) => {
-  res.send("admin got all data");
-});
-
-app.get("/admin/deleteuser", (req, res) => {
-  res.send("admin deleted the user");
-});
+app.use("/", (err, req, res, next)=>{
+if (err){
+    res.status(500).send("something went wrong")
+}
+})
 
 app.listen(3000, () => {
   console.log("server is running");
